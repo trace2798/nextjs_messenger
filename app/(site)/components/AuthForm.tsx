bade8ff9688e409c2461be5fb8786be734582288
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-05-08 16:54:09 +0530

    input field and button layout done

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,117 @@
+"use client";
+
+import Button from "@/app/components/Button";
+import Input from "@/app/components/input/Input";
+import { FC, useCallback, useState } from "react";
+import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+
+type Variant = "LOGIN" | "REGISTER";
+
+const AuthForm = () => {
+  const [variant, setVariant] = useState<Variant>();
+  const [isLoading, setIsLoading] = useState(false);
+  const toggleVariant = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("REGISTER");
+    } else {
+      setVariant("LOGIN");
+    }
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    if (variant === "REGISTER") {
+      //axios register
+    }
+    if (variant === "LOGIN") {
+      //NextAuth SignIn
+    }
+  };
+
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+
+    //NextAUth Social Action
+  };
+
+  return (
+    <>
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+          {/* we need to wrap our onSubmit with handleSubmit so that it gets the data  */}
+          <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+            {variant === "REGISTER" && (
+              <Input
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+                id="name"
+                label="Name"
+              />
+            )}
+            <Input
+              disabled={isLoading}
+              register={register}
+              errors={errors}
+              required
+              id="email"
+              label="Email address"
+              type="email"
+            />
+            <Input
+              disabled={isLoading}
+              register={register}
+              errors={errors}
+              required
+              id="password"
+              label="Password"
+              type="password"
+            />
+            <div>
+              <Button disabled={isLoading} fullWidth type="submit">
+                {variant === "LOGIN" ? "Sign in" : "Register"}
+              </Button>
+            </div>
+          </form>
+
+          <div className="mt-6">
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <div className="w-full border-t border-gray-300" />
+              </div>
+              <div className="relative flex justify-center text-sm">
+                <span className="bg-white px-2 text-gray-500"></span>
+              </div>
+            </div>
+          </div>
+          <div className="flex gap-2 justify-center text-sm mt-6 px-2  text-gray-500">
+            <div>
+              {variant === "LOGIN"
+                ? "New to Messenger?"
+                : "Already have an account?"}
+            </div>
+            <div onClick={toggleVariant} className="underline cursor-pointer">
+              {variant === "LOGIN" ? "Create an account" : "Login"}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AuthForm;
---
+++ app/components/Button.tsx
@@ -0,0 +1,46 @@
+import clsx from "clsx";
+
+interface ButtonProps {
+  type?: "button" | "submit" | "reset" | undefined;
+  fullWidth?: boolean;
+  children?: React.ReactNode;
+  onClick?: () => void;
+  secondary?: boolean;
+  danger?: boolean;
+  disabled?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  type = "button",
+  fullWidth,
+  children,
+  onClick,
+  secondary,
+  danger,
+  disabled,
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      type={type}
+      disabled={disabled}
+      className={clsx(
+        `
+        items-center justify-center w-full px-6 py-2.5 text-center text-white duration-200 bg-black border-2 border-black rounded-full nline-flex hover:bg-transparent hover:border-black hover:text-black focus:outline-none focus-visible:outline-black text-sm focus-visible:ring-black
+        `,
+        disabled && "opacity-50 cursor-default",
+        fullWidth && "w-full",
+        secondary ? "text-gray-900" : "text-white",
+        danger &&
+          "bg-rose-500 hover:bg-rose-600 focus-visible:outline-rose-600",
+        !secondary &&
+          !danger &&
+          "bg-sky-500 hover:bg-sky-600 focus-visible:outline-sky-600"
+      )}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
