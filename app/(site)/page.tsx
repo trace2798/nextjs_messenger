41d6fca73fd3b599287971eb9b66ca38961b9b34
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-05-15 14:10:45 +0530

    changed styling of hero message

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -6,7 +6,6 @@ import Head from "next/head";
 export default function Home() {
   return (
     <>
-      
       <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100 bg-gradient-to-r from-blue-100 via-blue-300 to-blue-500 dark:">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           {/* <Image
@@ -16,10 +15,8 @@ export default function Home() {
             src="/images/logo.png"
             alt="Logo"
           /> */}
-          <h2 className="mt-6 text-center text-4xl font-bold tracking-tight text-gray-900">
-            <span className="bg-clip-text text-transparent bg-gradient-to-t from-gray-900 to-gray-500">
-              Sign in to your account
-            </span>
+          <h2 className="mt-6 text-center text-4xl font-bold tracking-tight bg-clip-text text-transparent bg-gradient-to-t from-gray-900 to-gray-500">
+            Sign in to your account
           </h2>
         </div>
         <div className="flex justify-center mt-4">
